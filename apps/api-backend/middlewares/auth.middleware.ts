1b473ea3acbb347a63f15011f2a66b98bf6d8f9e
Soubhik Halder <soubhikhalder82@gmail.com>
2026-07-16 17:22:42 +0530

    feat(auth): enhance API key validation with caching and improved error messages

--- apps/api-backend/middlewares/auth.middleware.ts
+++ apps/api-backend/middlewares/auth.middleware.ts
@@ -2,35 +2,70 @@ import type { Request, Response , NextFunction} from "express";
 import { apikeysTable, db } from '@repo/db';
 import { eq } from "drizzle-orm";
 import { hashToken } from "../libs/hash";
+import { redis } from "@repo/redis";
 
+const API_KEY_CACHE_TTL = 600;
+const API_KEY_REGEX = /^sk_[A-Za-z0-9_-]{64}$/;
 
 export async function auth(req:Request,res:Response,next:NextFunction){
-    const apiKey = req.headers["x-api-key"] as string;
+    const apiKey = req.get("X-API-Key");
     // validation
     if(!apiKey){
         return res.status(400).json({
-            message:"Please send a valid ApiKey"
+            message:"Invalid API key"
         })
     }
-    // Authentication
+    
+    if (!API_KEY_REGEX.test(apiKey)) {
+        return res.status(401).json({
+            message: "Invalid API key",
+        });
+    }
+    const hashedKey = hashToken(apiKey);
     try {
-        const [info] = await db.select({id:apikeysTable.id,userId:apikeysTable.userId}).from(apikeysTable).where(eq(apikeysTable.key_hash,hashToken(apiKey)))
-        if(!info){
-            return res.status(401).json({
-                message:"Please send a valid ApiKey"
-            })
+
+        const cache = await redis.get(`apiKey:${hashedKey}`);
+    
+        if(cache === null){
+            // Cache miss
+            const [info] = await db
+            .select({id:apikeysTable.id,userId:apikeysTable.userId})
+            .from(apikeysTable)
+            .where(eq(apikeysTable.key_hash,hashedKey));
+    
+    
+            if(!info){
+                return res.status(401).json({
+                    message:"Invalid API key"
+                })
+            }
+            // cache the ApiKey  with a TTL of 5 mins
+            await redis.setEx(`apiKey:${hashedKey}`,API_KEY_CACHE_TTL,JSON.stringify({
+                apiKeyId: info.id,
+                userId: info.userId,
+            }));
+            req.apiKey = {
+                id:info.id,
+                apiKey:apiKey,
+                userId:info.userId
+            }
+            return next();
+        }else{
+            // Cache hit
+            const key_details = JSON.parse(cache)
+
+            req.apiKey = {
+                id:key_details.apiKeyId,
+                apiKey:apiKey,
+                userId:key_details.userId
+            }
+            return next();
         }
-        req.apiKey = {
-            id:info.id,
-            apiKey:apiKey,
-            userId:info.userId
-        }
-        return next();
     } catch (error) {
         console.error(error);
 
         return res.status(500).json({
             message:"Internal Server Error"
-        })
+        });
     }
 }
